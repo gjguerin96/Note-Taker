9af0fc9883da532c366c2bccf9c47999946cbcc6
Gabriel Guerin <gjguerin96@gmail.com>
2021-11-30 19:47:24 -0800

    connected frontend and backend

---
+++ helpers/fsUtils.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => {
+    if (err) throw err
+  });
+
+const readAndAppend = (content, file) => {
+  console.log(content)
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
--- server.js
+++ server.js
@@ -1,20 +1,52 @@
 const express = require('express')
 const path = require('path')
+const noteData = require('./public/db/db.json')
+const uuid = require('./helpers/uuid')
+const {
+    readFromFile,
+    readAndAppend,
+  //   writeToFile,
+  } = require('./helpers/fsUtils.js');
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-)
+app.use(express.static('public'))
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
-)
+);
+
+// app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.get('/api/notes', (req,res) => {
+    readFromFile('./public/db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req,res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+        title,
+        text,
+        id: uuid()
+    };
+
+    readAndAppend(newNote, './public/db/db.json');
+  }
+  
+  readFromFile('./public/db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
-)
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
